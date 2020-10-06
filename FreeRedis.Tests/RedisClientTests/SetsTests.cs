5405c7a191d38ac3077833a6bcc785dfd3336449
28810 <28810@YEXIANGQIN>
2020-10-06 20:29:25 +0800

    add Sets Tests

---
+++ FreeRedis.Tests/RedisClientTests/SetsTests.cs
@@ -0,0 +1,142 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using Xunit;
+
+namespace FreeRedis.Tests.RedisClientTests
+{
+    public class SetsTests
+    {
+
+        [Fact]
+        public void SAdd()
+        {
+            using (var cli = Util.GetRedisClient())
+            {
+
+            }
+        }
+
+        [Fact]
+        public void SCard()
+        {
+            using (var cli = Util.GetRedisClient())
+            {
+
+            }
+        }
+
+        [Fact]
+        public void SDiff()
+        {
+            using (var cli = Util.GetRedisClient())
+            {
+
+            }
+        }
+
+        [Fact]
+        public void SDiffStore()
+        {
+            using (var cli = Util.GetRedisClient())
+            {
+
+            }
+        }
+
+        [Fact]
+        public void SInter()
+        {
+            using (var cli = Util.GetRedisClient())
+            {
+
+            }
+        }
+
+        [Fact]
+        public void SInterStore()
+        {
+            using (var cli = Util.GetRedisClient())
+            {
+
+            }
+        }
+
+        [Fact]
+        public void SIsMember()
+        {
+            using (var cli = Util.GetRedisClient())
+            {
+
+            }
+        }
+
+        [Fact]
+        public void SMeMembers()
+        {
+            using (var cli = Util.GetRedisClient())
+            {
+
+            }
+        }
+
+        [Fact]
+        public void SMove()
+        {
+            using (var cli = Util.GetRedisClient())
+            {
+
+            }
+        }
+
+        [Fact]
+        public void SPop()
+        {
+            using (var cli = Util.GetRedisClient())
+            {
+
+            }
+        }
+
+        [Fact]
+        public void SRandMember()
+        {
+            using (var cli = Util.GetRedisClient())
+            {
+
+            }
+        }
+
+        [Fact]
+        public void SRem()
+        {
+            using (var cli = Util.GetRedisClient())
+            {
+
+            }
+        }
+
+        [Fact]
+        public void SUnion()
+        {
+            using (var cli = Util.GetRedisClient())
+            {
+
+            }
+        }
+
+        [Fact]
+        public void SUnionStore()
+        {
+            using (var cli = Util.GetRedisClient())
+            {
+
+            }
+        }
+
+    }
+}
--- FreeRedis/RedisClient/Sets.cs
+++ FreeRedis/RedisClient/Sets.cs
@@ -7,22 +7,46 @@ namespace FreeRedis
 {
 	partial class RedisClient
 	{
-		public RedisResult<long> SAdd(string key, params string[] members) => Call<long>("SADD", key, members);
-		public RedisResult<long> SCard(string key) => Call<long>("SCARD", key);
-		public RedisResult<string[]> SDiff(params string[] keys) => Call<string[]>("SDIFF", null, keys);
-		public RedisResult<long> SDiffStore(string destination, params string[] keys) => Call<long>("SDIFFSTORE", destination, keys);
-		public RedisResult<string[]> SInter(params string[] keys) => Call<string[]>("SINTER", null, keys);
-		public RedisResult<long> SInterStore(string destination, params string[] keys) => Call<long>("SINTERSTORE", destination, keys);
-		public RedisResult<bool> SIsMember(string key, string member) => Call<bool>("SISMEMBER", key, member);
-		public RedisResult<string[]> SMeMembers(string key) => Call<string[]>("SMEMBERS", key);
-		public RedisResult<bool> SMove(string source, string destination, string member) => Call<bool>("SMOVE", source, destination, member);
-		public RedisResult<string> SPop(string key) => Call<string>("SPOP", key);
-		public RedisResult<string[]> SPop(string key, int count) => Call<string[]>("SPOP", key, count);
-		public RedisResult<string> SRandMember(string key) => Call<string>("SRANDMEMBER", key);
-		public RedisResult<string[]> SRandMember(string key, int count) => Call<string[]>("SRANDMEMBER", key, count);
-		public RedisResult<long> SRem(string key, params string[] members) => Call<long>("SREM", key, members);
+		public long SAdd(string key, params object[] members) => Call<long>("SADD", key, members.Select(a => SerializeRedisValue(a)).ToArray()).ThrowOrValue();
+		public long SCard(string key) => Call<long>("SCARD", key).ThrowOrValue();
+
+		public string[] SDiff(params string[] keys) => Call<string[]>("SDIFF", null, keys).ThrowOrValue();
+		public T[] SDiff<T>(params string[] keys) => SReadArray<T>("SDIFF", keys);
+		public long SDiffStore(string destination, params string[] keys) => Call<long>("SDIFFSTORE", destination, keys).ThrowOrValue();
+
+		public string[] SInter(params string[] keys) => Call<string[]>("SINTER", null, keys).ThrowOrValue();
+		public T[] SInter<T>(params string[] keys) => SReadArray<T>("SINTER", keys);
+		public long SInterStore(string destination, params string[] keys) => Call<long>("SINTERSTORE", destination, keys).ThrowOrValue();
+
+		public bool SIsMember(string key, object member) => Call<bool>("SISMEMBER", key, SerializeRedisValue(member)).ThrowOrValue();
+		public string[] SMeMembers(string key) => Call<string[]>("SMEMBERS", key).ThrowOrValue();
+		public T[] SMeMembers<T>(string key) => SReadArray<T>("SDIFF", key);
+
+		public bool SMove(string source, string destination, object member) => Call<bool>("SMOVE", source, destination, SerializeRedisValue(member)).ThrowOrValue();
+
+		public string SPop(string key) => Call<string>("SPOP", key).ThrowOrValue();
+		public T SPop<T>(string key) => Call<byte[]>("SPOP", key).NewValue(a => DeserializeRedisValue<T>(a)).ThrowOrValue();
+		public string[] SPop(string key, int count) => Call<string[]>("SPOP", key, count).ThrowOrValue();
+		public T[] SPop<T>(string key, int count) => SReadArray<T>("SPOP", key, count);
+
+		public string SRandMember(string key) => Call<string>("SRANDMEMBER", key).ThrowOrValue();
+		public T SRandMember<T>(string key) => Call<byte[]>("SRANDMEMBER", key).NewValue(a => DeserializeRedisValue<T>(a)).ThrowOrValue();
+		public string[] SRandMember(string key, int count) => Call<string[]>("SRANDMEMBER", key, count).ThrowOrValue();
+		public T[] SRandMember<T>(string key, int count) => SReadArray<T>("SRANDMEMBER", key, count);
+
+		public long SRem(string key, params object[] members) => Call<long>("SREM", key, members.Select(a => SerializeRedisValue(a)).ToArray()).ThrowOrValue();
 		//SSCAN key cursor [MATCH pattern] [COUNT count]
-		public RedisResult<string[]> SUnion(params string[] keys) => Call<string[]>("SUNION", null, keys);
-		public RedisResult<long> SUnionStore(string destination, params string[] keys) => Call<long>("SUNIONSTORE", destination, keys);
-    }
+
+		public string[] SUnion(params string[] keys) => Call<string[]>("SUNION", null, keys).ThrowOrValue();
+		public T[] SUnion<T>(params string[] keys) => SReadArray<T>("SUNION", keys);
+		public long SUnionStore(string destination, params string[] keys) => Call<long>("SUNIONSTORE", destination, keys).ThrowOrValue();
+
+		T[] SReadArray<T>(string cmd, params object[] parms)
+		{
+			CallWriteOnly(cmd, null, parms);
+			var value = Resp3Helper.Read<object>(Stream).ThrowOrValue();
+			var list = value.ConvertTo<byte[][]>();
+			return list.Select(a => DeserializeRedisValue<T>(a)).ToArray();
+		}
+	}
 }
