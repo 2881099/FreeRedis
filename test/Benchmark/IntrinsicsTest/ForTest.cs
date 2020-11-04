8a78ae479f2d1376738e2a43822689ae381f8787
AzulX <16236903+NMSAzulX@users.noreply.github.com>
2020-11-04 18:42:15 +0800

    增加指令运算测试
    增加指令运算测试，备数组操作。

---
+++ test/Benchmark/IntrinsicsTest/ForTest.cs
@@ -0,0 +1,42 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+using System.Threading;
+
+namespace IntrinsicsTest
+{
+    [MemoryDiagnoser, CoreJob, MarkdownExporter, RPlotExporter]
+    [MinColumn, MaxColumn, MeanColumn, MedianColumn]
+    public class ForTest
+    {
+        public const long Increment = 36;
+        public void Intrinsics()
+        {
+            //Interlocked.Exchange(,)
+
+            //   var result = new Vector<long>(0);
+            //var offset = new Vector<long>(0);
+            //var max = new Vector<long>(1000);
+            //long temp = offset.CopyTo(temp);
+            //while ()
+            //{
+
+            //}
+            //for (int i = 0; i < 1000; i++)
+            //{
+            //    result += Increment + i;
+            //}
+        }
+
+        public void Normal()
+        {
+            //long result = 0;
+            //for (int i = 0; i < 1000; i++)
+            //{
+            //    result += Increment + i;
+            //}
+        }
+    }
+}
---
+++ test/Benchmark/IntrinsicsTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IntrinsicsTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
