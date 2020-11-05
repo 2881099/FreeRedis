3415cf4ff0860b7d6ec4ec3340520366d03b9b36
AzulX <16236903+NMSAzulX@users.noreply.github.com>
2020-11-05 15:56:54 +0800

    Update SocketUnitTest.cs
    增加例子的描述

--- test/Unit/SocketTest/SocketUnitTest.cs
+++ test/Unit/SocketTest/SocketUnitTest.cs
@@ -13,7 +13,7 @@ namespace SocketTest
     {
 
         public static string Result;
-        [Fact]
+        [Fact(DisplayName ="测试 SOCKET 在平台的可用性")]
         public void Test1()
         {
             var point = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 18989);
@@ -26,16 +26,16 @@ namespace SocketTest
         {
             TcpListener listener = new TcpListener(point);
             listener.Start();
-            Console.WriteLine("Start Accept!");
+            Console.WriteLine("Server: Start Accept!");
             var client = await listener.AcceptTcpClientAsync();
-            Console.WriteLine("Accept One!");
+            Console.WriteLine("Server: Accept One!");
             byte[] bytes = new byte[1024];
 
             int count = 0;
             while (count<20)
             {
                 await Task.Delay(1000);
-                Console.WriteLine("loop!");
+                Console.WriteLine("Server: Loop!");
                 NetworkStream stream = client.GetStream();
                 int i;
                 if ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
@@ -43,7 +43,7 @@ namespace SocketTest
                     var data = Encoding.UTF8.GetString(bytes, 0, i);
                     Assert.Equal("test", data);
                     client.Client.Send(bytes);
-                    //Console.WriteLine("客户端发来：" + data);
+                    Console.WriteLine("Server: Data has been send!");
                     if (data == "test")
                     {
                         stream.Dispose();
@@ -56,15 +56,17 @@ namespace SocketTest
 
         private async void StartClient(IPEndPoint point)
         {
-            await Task.Delay(2000);
-            var endpoit = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 18989);
+            await Task.Delay(3000);
             SocketConnectionFactory client = new SocketConnectionFactory(new SocketTransportOptions());
-            var connection = client.ConnectAsync(endpoit).Result;
+            var connection = client.ConnectAsync(point).Result;
+            Console.WriteLine("Client: Connected!");
             var buffer = Encoding.UTF8.GetBytes("test");
             await connection.Transport.Output.WriteAsync(buffer);
+            Console.WriteLine("Client: Data has been send!");
             while (Result==default)
             {
                 Thread.Sleep(1000);
+                Console.WriteLine("Client: Loop!");
             }
             Assert.Contains("test", Result);
             await client.DisposeAsync();
