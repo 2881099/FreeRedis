b682b42797023a45b12d210606a7fe88565c4885
AzulX <16236903+NMSAzulX@users.noreply.github.com>
2020-11-11 15:10:05 +0800

    Update Program.cs

--- examples/console_netcore31_newsocket/Program.cs
+++ examples/console_netcore31_newsocket/Program.cs
@@ -25,7 +25,7 @@ namespace console_netcore31_newsocket
         {
             //FreeRedisTest();
             //123.57.78.153:9379
-            //var endpoit = new IPEndPoint(IPAddress.Parse("123.57.78.153"), 9379);
+            //var endpoit = new IPEndPoint(IPAddress.Parse(),);
             var endpoit = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 9379);
             //NewSocketTest(endpoit);
 
@@ -137,7 +137,7 @@ namespace console_netcore31_newsocket
             //ResultDict = new ConcurrentDictionary<string, string>();
             SocketConnectionFactory client = new SocketConnectionFactory(new SocketTransportOptions());
             var connection = client.ConnectAsync(endpoit).Result;
-            connection.Transport.Output.WriteAsync(Encoding.UTF8.GetBytes("AUTH 0f649985e1ae10a\r\n"));
+            connection.Transport.Output.WriteAsync(Encoding.UTF8.GetBytes("AUTH \r\n"));
             var result = connection.Transport.Output.WriteAsync(Encoding.UTF8.GetBytes("SELECT 15\r\n")).Result;
             Thread.Sleep(3000);
             var readResult = connection.Transport.Input.ReadAsync().Result;
@@ -207,7 +207,7 @@ namespace console_netcore31_newsocket
         public static async void FreeRedisTest()
         {
 
-            var client = new RedisClient("123.57.78.153:9379,password=0f649985e1ae10a,database=15,asyncPipeline=true");
+            var client = new RedisClient("");
             SendPing(client);
             while (count != frequence)
             {
