a7fa42a763902cb4685a34f2423e6bcb7e453bdd
htrlq <2598145226@qq.com>
2020-11-26 15:16:12 +0800

    add build cli

---
+++ src/FreeRedis.Build.Cli/Template/TemplateClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FreeRedis.Build.Cli.Template
+{
+    public class TemplateClass
+    {
+        public List<string> Methods;
+
+        public TemplateClass(List<string> methods)
+        {
+            Methods = methods;
+        }
+    }
+}
