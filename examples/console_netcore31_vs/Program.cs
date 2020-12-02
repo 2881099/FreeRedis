799611ce2ac9b0c8bb58c9f16c0145b7d88d3254
2881099 <2881099@qq.com>
2020-12-02 09:22:09 +0800

    update demo

--- examples/console_netcore31_vs/Program.cs
+++ examples/console_netcore31_vs/Program.cs
@@ -209,7 +209,7 @@ namespace console_netcore31_vs
                     var val = pipe.Get(tmp);
                 }
                 var vals = pipe.EndPipe();
-                for (var a = 0; a < 100000; a ++)
+                for (var a = 1; a < 200000; a += 2)
                 {
                     var val = vals[a].ToString();
                     if (val != String) throw new Exception("not equal");
