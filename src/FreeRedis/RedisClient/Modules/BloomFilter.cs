8d2bd88010bc6180ff6fb6780d115b3cf5ebe1ee
lhw <t.seven@sino-commercial.com>
2022-11-02 20:32:38 +0800

    fix: 1. ConnectionStringBuilder的Host参数兼容localhost为ipv6的情况;2.  Module模块方法copy同步方法修改来提供异步方法入口;

--- src/FreeRedis/RedisClient/Modules/BloomFilter.cs
+++ src/FreeRedis/RedisClient/Modules/BloomFilter.cs
@@ -2,11 +2,43 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace FreeRedis
 {
     partial class RedisClient
     {
+#if isasync
+        #region async (copy from sync)
+
+        public Task<string> BfReserveAsync(string key, decimal errorRate, long capacity, int expansion = 2, bool nonScaling = false) => CallAsync("BF.RESERVE"
+            .InputKey(key)
+            .Input(errorRate, capacity)
+            .InputIf(expansion != 2, "EXPANSION", expansion)
+            .InputIf(nonScaling, "NONSCALING"), rt => rt.ThrowOrValue<string>());
+        public Task<bool> BfAddAsync(string key, string item) => CallAsync("BF.ADD".InputKey(key, item), rt => rt.ThrowOrValue<bool>());
+        public Task<bool[]> BfMAddAsync(string key, string[] items) => CallAsync("BF.MADD".InputKey(key, items), rt => rt.ThrowOrValue<bool[]>());
+
+        public Task<string> BfInsertAsync(string key, string[] items, long? capacity = null, string error = null, int expansion = 2, bool noCreate = false, bool nonScaling = false) => CallAsync("BF.INSERT"
+            .InputKey(key)
+            .InputIf(capacity != null, "CAPACITY", capacity)
+            .InputIf(!string.IsNullOrWhiteSpace(error), "ERROR", error)
+            .InputIf(expansion != 2, "EXPANSION", expansion)
+            .InputIf(noCreate, "NOCREATE")
+            .InputIf(nonScaling, "NONSCALING")
+            .Input("ITEMS", items), rt => rt.ThrowOrValue<string>());
+
+        public Task<bool> BfExistsAsync(string key, string item) => CallAsync("BF.EXISTS".InputKey(key, item), rt => rt.ThrowOrValue<bool>());
+        public Task<bool[]> BfMExistsAsync(string key, string[] items) => CallAsync("BF.MEXISTS".InputKey(key, items), rt => rt.ThrowOrValue<bool[]>());
+        public Task<ScanResult<byte[]>> BfScanDumpAsync(string key, long iter) => CallAsync("BF.SCANDUMP".InputKey(key, iter), rt => rt.ThrowOrValue((a, _) =>
+            new ScanResult<byte[]>(a[0].ConvertTo<long>(), a[1].ConvertTo<byte[][]>())));
+
+        public Task<string> BfLoadChunkAsync(string key, long iter, byte[] data) => CallAsync("BF.LOADCHUNK".InputKey(key, iter).InputRaw(data), rt => rt.ThrowOrValue<string>());
+        public Task<Dictionary<string, string>> BfInfoAsync(string key) => CallAsync("BF.INFO".InputKey(key), rt => rt.ThrowOrValue((a, _) => a.MapToHash<string>(rt.Encoding)));
+
+        #endregion
+#endif
+
         public string BfReserve(string key, decimal errorRate, long capacity, int expansion = 2, bool nonScaling = false) => Call("BF.RESERVE"
             .InputKey(key)
             .Input(errorRate, capacity)
--- src/FreeRedis/RedisClient/Modules/RedisBloomCountMinSketch.cs
+++ src/FreeRedis/RedisClient/Modules/RedisBloomCountMinSketch.cs
@@ -2,11 +2,32 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace FreeRedis
 {
     partial class RedisClient
     {
+#if isasync
+        #region async (copy from sync)
+
+        public Task<string> CmsInitByDimAsync(string key, long width, long depth) => CallAsync("CMS.INITBYDIM".InputKey(key, width, depth), rt => rt.ThrowOrValue<string>());
+        public Task<string> CmsInitByProbAsync(string key, decimal error, decimal probability) => CallAsync("CMS.INITBYPROB".InputKey(key, error, probability), rt => rt.ThrowOrValue<string>());
+
+        public Task<long> CmsIncrByAsync(string key, string item, long increment) => CallAsync("CMS.INCRBY".InputKey(key, item, increment), rt => rt.ThrowOrValue(a => a.ConvertTo<long[]>().FirstOrDefault()));
+        public Task<long[]> CmsIncrByAsync(string key, Dictionary<string, long> itemIncrements) => CallAsync("CMS.INCRBY".InputKey(key).InputKv(itemIncrements, false, SerializeRedisValue), rt => rt.ThrowOrValue<long[]>());
+
+        public Task<long[]> CmsQueryAsync(string key, string[] items) => CallAsync("CMS.QUERY".InputKey(key, items), rt => rt.ThrowOrValue<long[]>());
+        public Task<string> CmsMergeAsync(string dest, long numKeys, string[] src, long[] weights) => CallAsync("CMS.MERGE"
+            .InputKey(dest, numKeys)
+            .InputKey(src)
+            .InputIf(weights?.Any() == true, "WEIGHTS", weights), rt => rt.ThrowOrValue<string>());
+
+        public Task<Dictionary<string, string>> CmsInfoAsync(string key) => CallAsync("CMS.INFO".InputKey(key), rt => rt.ThrowOrValue((a, _) => a.MapToHash<string>(rt.Encoding)));
+
+        #endregion
+#endif
+
         public string CmsInitByDim(string key, long width, long depth) => Call("CMS.INITBYDIM".InputKey(key, width, depth), rt => rt.ThrowOrValue<string>());
         public string CmsInitByProb(string key, decimal error, decimal probability) => Call("CMS.INITBYPROB".InputKey(key, error, probability), rt => rt.ThrowOrValue<string>());
 
